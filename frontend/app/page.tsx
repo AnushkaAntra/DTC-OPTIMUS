1c6c0b5a66675a1cf14fdab97114b44fd30b0f30
Anshika-001 <anshikaanand02@gmail.com>
2024-09-01 01:29:38 +0530

    [Redundant] changes to page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,12 @@
-
-
 import type { NextPage } from "next";
 import Navbar from "@/components/custom/navbar";
-import FrameComponent from "@/components/custom/frame-component";
-import Features from "@/components/custom/features";
-import FrameComponent1 from "@/components/custom/frame-component1";
-import styles from "@/components/custom/index.module.css";
+// import "@/components/custom/styles/navbar.module.css";
 
 const LandingPage: NextPage = () => {
   return (
-    <div className={styles.landingPage}>
-      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
-      <section className={styles.content}>
-        <Navbar />
-        <FrameComponent />
-      </section>
-      <section className={styles.featuresWrapper}>
-        <Features />
-      </section>
-      <FrameComponent1 />
-    </div>
+    <>
+    <Navbar />
+    </>
   );
 };
 export default LandingPage;
