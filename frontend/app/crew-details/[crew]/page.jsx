f7e042fefbb2972af0b1eed2c9662c6efdfb787b
AnushkaAntra <antraanushka31@gmail.com>
2024-09-01 14:34:41 +0530

    updated crew-details

--- frontend/app/crew-details/[crew]/page.jsx
+++ frontend/app/crew-details/[crew]/page.jsx
@@ -27,19 +27,27 @@ export default async function CrewDetails({ params }) {
           <tbody>
             <tr>
               <td className="py-3 px-4 border-b border-gray-200 font-medium">Name</td>
-              <td className="py-3 px-4 border-b border-gray-200">{data.driver_name}</td> {/* Assuming data structure */}
+              <td className="py-3 px-4 border-b border-gray-200">{data.driver_name}</td> 
             </tr>
             <tr>
               <td className="py-3 px-4 border-b border-gray-200 font-medium">License Number</td>
-              <td className="py-3 px-4 border-b border-gray-200">{data.license_no}</td> {/* Assuming data structure */}
+              <td className="py-3 px-4 border-b border-gray-200">{data.license_no}</td> 
             </tr>
             <tr>
               <td className="py-3 px-4 border-b border-gray-200 font-medium">Phone_no</td>
-              <td className="py-3 px-4 border-b border-gray-200">{data.ph_no}</td> {/* Assuming data structure */}
+              <td className="py-3 px-4 border-b border-gray-200">{data.ph_no}</td> 
             </tr>
             <tr>
               <td className="py-3 px-4 border-b border-gray-200 font-medium">Address</td>
-              <td className="py-3 px-4 border-b border-gray-200">{data.address}</td> {/* Assuming data structure */}
+              <td className="py-3 px-4 border-b border-gray-200">{data.address}</td> 
+            </tr>
+            <tr>
+              <td className="py-3 px-4 border-b border-gray-200 font-medium">status</td>
+              <td className="py-3 px-4 border-b border-gray-200">{data.status = "available" ? 
+                <div className='bg-green-500 text-green-500 w-5 h-5 rounded-full'></div> 
+                : <div className='bg-red-500 text-red-500 w-5 h-5 rounded-full'></div>  
+                 }
+              </td> 
             </tr>
           </tbody>
         </table>
--- frontend/app/crew-details/page.jsx
+++ frontend/app/crew-details/page.jsx
@@ -15,7 +15,7 @@ export default async function CrewDetails() {
         <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
           <h2 className='text-xl font-semibold text-secondary bg-primary py-3 px-4'>Available Crews</h2>
           <div className='h-80 overflow-y-auto'>
-            {available.length > 0 ? (
+            {available.status = 'available' ? (
               available.map(driver => (
                 <Link href={'/crew-details/' + driver._id} key={driver._id}>
                   <div className="block px-4 py-3 mb-2 border-l-4 border-secondary bg-gray-100 hover:bg-gray-300 transition-colors cursor-pointer">
@@ -33,7 +33,7 @@ export default async function CrewDetails() {
         <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
           <h2 className='text-xl font-semibold text-secondary bg-primary py-3 px-4'>Not Available Crews</h2>
           <div className='h-96 overflow-y-auto'>
-            {notAvailable.length > 0 ? (
+            {notAvailable.status != 'available' ? (
               notAvailable.map(driver => (
                 <Link href={'/crew-details/' + driver._id} key={driver._id}>
                   <div className="block px-4 py-3 mb-2 border-l-4 border-secondary bg-gray-100 hover:bg-gray-300 transition-colors cursor-pointer">
